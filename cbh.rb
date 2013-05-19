bf124ccdc6d6c6d87959036489d77038a2bbc378
Christian Grebe <christian.grebe@gmx.net>
2013-05-19 07:51:49 +0200

    modified:   cbh.rb Design changes. Too much frickeling with positioning but whatever...

--- cbh.rb
+++ cbh.rb
@@ -1,29 +1,40 @@
 
 
 
-Shoes.app :title => "Clipboard History" do
+Shoes.app :title => "Clipboard History", :width => 700, :height => 700, :resizable => false do
+  background dimgray
+  #border white, :strokewidth => 5
+  
   @index = 0
   @elements = clipboard() ? [clipboard()] : [0]
 
+    
+  
+  stack :height => 32, :width => "100%" do
+    background darkblue
+    border gainsboro, :strokewidth => 3
+    @title = para "Clipboard entry %03d" % (@index + 1), :size => 16, 
+    :stroke => white, :align => "center", :weight => 900
+  end
 
-  stack do
-    background dimgray
-    border gray, :strokewidth => 3
-    @title = title "Clipboard entry %03d" % (@index + 1), :size => 16, 
-    :stroke => blue, :align => "center", :weight => 900, :margin => 10
-  end
-  stack do
-    background darkgray
-    border gray, :strokewidth => 3
-    @clipboard = para "#{@elements[@index]}", :stroke => blue
-  end
-  
-  flow :left => 20 do
-    @prev = button "Prev"#, :left => 25
-    @next = button "Next"
-    @copy = button "Copy to clipboard"
+  stack :width => 350, :margin => 5 do
+    #background gray
+    #border dimgray, :strokewidth => 2
+    flow :left => 100 do #:left => 100, :right => 100
+      @prev = button "Prev", :width => "33%"
+      @next = button "Next", :width => "33%"
+      @copy = button "Copy to clipboard", :width => "34%"
+    end
+    #self.move(5,32)
   end
   
+  stack :width => 700 do
+    background silver
+    border gainsboro, :strokewidth => 10
+    @clipboard = para "#{@elements[@index]}", :stroke => black, :size => 10, :left => 85, :top => 20
+  end
+  
+
   
   @prev.click do
     @index -= 1 if @index > 0
@@ -41,9 +52,12 @@ Shoes.app :title => "Clipboard History" do
     app.clipboard = @elements[@index]
   end
 
-  animate 1 do
-    @elements << clipboard() if clipboard() != @elements.last
-    
+  animate(3) do
+    if clipboard() != @elements.last
+      @elements << clipboard()
+      @index = @elements.length - 1
+      @clipboard.text = @elements[@index]
+      @title.text = "Clipboard entry %03d" % (@index + 1)
+    end
   end
-
 end
